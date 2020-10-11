a49f1a68b792649c2a9953c07b2c727d4277b8be
Olenca <Alvarado0lenca@gmail.com>
2020-10-10 17:34:23 -0700

    actualizando usuario 4 vez

--- routes/factura.js
+++
@@ -1,5 +0,0 @@
-
-//se ara de nuevo
-
-
-
--- routes/user.js
+++ routes/user.js
@@ -1,57 +1,105 @@
-var express = require("express");
-var sha1 = require("sha1");
-var router = express.Router();
+var express = require('express');
+
 var Users = require("../database/users");
 var Cliente = require("../database/cliente");
+var router = express.Router();
 var jwt = require("jsonwebtoken");
-//POST
-router.post("/login", async(req, res) => {
-  var body = req.body;
-    if (body.email == null) {
-        res.status(300).json({msn: "El email es necesario"});
-             return;
+
+//Registrar usuario
+router.post('/user', (req, res) => {
+    var params = req.body;
+    var email = params.email;
+    if(params.TipoUsuario != null){
+        params["TipoUsuario"] = "Propietario del Restaurant";
+        console.log("se registro al propietario de restaurant");
+    }else{
+        params["TipoUsuario"] = "cliente";
+        console.log("se registro cliente");
     }
-    if (body.password == null) {
-        res.status(300).json({msn: "El password es necesario"});
-        return;
-    }
-  var result = await Cliente.findOne({email: body.email, password: sha1(body.password)}).exec((err, doc) => {
-    if (err) {
-      res.status(300).json({ msn : "No se puede concretar con la peticion" });
-      return;
-    }
-    console.log(doc);
-    if (doc) {
-       console.log(result);
-      jwt.sign({name: doc.email, password: doc.password}, "secretkey123", (err, token) => {
-          console.log(result);
-          res.status(200).json({
-            resp:200,
-            token : token,
-            dato:doc
-          });
-      })
-    } else {
-      res.status(400).json({ msn : "El usuario no existe en la base de datos"});
-    }
- });
-});
-//Middelware
-function verifytoken (req, res, next) {
-  //Recuperar el header
-  const header = req.headers["authorization"];
-  if (header  == undefined) {
-      res.status(403).json({ msn: "No autorizado"});
-      return;
-  } else {
-      req.token = header.split(" ")[1];
-      jwt.verify(req.token, "secretkey123", (err, authData) => {
-        if (err) {
-          res.status(403).json({ msn: "No autorizado"});
-        } else {
-          next();
+    
+    Users.find({email: email}).exec( async(err, docs) => {
+        if(docs != null){
+            console.log("el email ya existe");
+            res.status(200).json({
+                msn: "el usuario ya existe"
+            });
+        }else{
+            params["registerdate"] = new Date();
+            var users = new USER(params);
+            var result = await users.save();
+            res.status(200).json(result);
+            console.log('user saved');
         }
+    });   
+});
+
+// GET Users
+router.get('/user', (req, res) => {
+    var params = req.query;
+    var limit = 100;
+    if (params.limit != null) {
+        limit = parseInt(params.limit);
+    } 
+    var skip = 0;
+    if (params.skip != null) {
+        skip = parseInt(params.skip);
+    }
+    Users.find({}).limit(limit).skip(skip).exec((err, docs) => {
+        res.status(200).json(docs);
+    console.log('mostrando users');
+    });
+});
+
+// DELETE User 
+router.delete("/user", async(req,res) => {
+    var id = req.query.id;
+    console.log(req.query.id);
+    if (id == null) {
+      res.status(300).json({
+        msn: "introducir id"    
       });
-  }
-}
-module.exports = router;
+      return;
+    }
+    var result = await Users.remove({_id: id});
+    res.status(200).json(result);
+    console.log('user deleted');
+  });
+
+//LOGIN
+router.post('/login', async(req, res, next)=>{
+    var params = req.body;
+    Users.find({email:params.email, password:params.password}).exec((err, docs)=>{
+        if (err){
+            res.status(300).json({
+                "msn": "Problemas con la base de datos"
+            });
+            return;
+        }
+        if (docs.length == 0){
+            res.status(300).json({
+                "msn": "Usuario y/o Password Incorrecto"
+            });
+            return;
+        } else {
+            //cracion del token
+            jwt.sign({name: params.email, password: params.password}, "password", (err, token)=>{
+                if(err){
+                    res.status(300).json({
+                        "msn": "Error con JSONWEBTOKEN"
+                    });
+                    return;
+                }
+                res.status(200).json({
+                    tipo:docs.tipo,
+                    "token": token
+                });
+                return;
+            });
+        }
+    });
+});
+
+
+
+
+module.exports = router
